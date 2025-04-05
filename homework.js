a537fc9986edc356b3dfa262dbcc8ce88d62ea27
Zaliron <104909873+Zaliron@users.noreply.github.com>
2025-04-05 23:58:54 +0500

    сфыы

--- homework.js
+++ homework.js
@@ -11,36 +11,9 @@ const HomeworkManager = {
         localStorage.setItem('homework', JSON.stringify(data));
     },
 
-    // Загрузка списка групп
-    getGroups() {
-        const groups = localStorage.getItem('groups');
-        return groups ? JSON.parse(groups) : [
-            '9ИСП-12К-24', '9КСК-10-24', 
-            '9ИСП-111К-24', '9ИКСС-13-24'
-        ];
-    },
-
-    // Сохранение списка групп
-    saveGroups(groups) {
-        localStorage.setItem('groups', JSON.stringify(groups));
-    },
-
-    // Добавление новой группы
-    addGroup(groupName) {
-        const groups = this.getGroups();
-        if (!groups.includes(groupName)) {
-            groups.push(groupName);
-            this.saveGroups(groups);
-            
-            // Инициализируем данные для новой группы
-            const data = this.loadHomework();
-            data[groupName] = this.initializeGroupData();
-            this.saveHomework(data);
-        }
-    },
-
-    // Инициализация данных для одной группы
-    initializeGroupData() {
+    // Инициализация пустой структуры данных
+    initializeEmptyData() {
+        const groups = ['9ИСП-12К-24', '9КСК-10-24', '9ИСП-111К-24', '9ИКСС-13-24'];
         const subjects = [
             'Башкирский язык', 'Биология', 'География', 'Иностранный язык',
             'Информатика', 'История', 'Литература', 'Математика',
@@ -49,25 +22,20 @@ const HomeworkManager = {
             'Физика', 'Физическая культура', 'Химия'
         ];
 
-        const groupData = {};
-        subjects.forEach(subject => {
-            groupData[subject] = {
-                text: "Пока нет заданий",
-                files: [],
-                date: new Date().toLocaleString('ru-RU'),
-                author: "System"
-            };
-        });
-        groupData.files = [];
-        return groupData;
-    },
-
-    // Инициализация пустой структуры данных
-    initializeEmptyData() {
         const data = {};
-        this.getGroups().forEach(group => {
-            data[group] = this.initializeGroupData();
+        groups.forEach(group => {
+            data[group] = {};
+            subjects.forEach(subject => {
+                data[group][subject] = {
+                    text: "Пока нет заданий",
+                    files: [],
+                    date: new Date().toLocaleString('ru-RU'),
+                    author: "System"
+                };
+            });
+            data[group].files = [];
         });
+
         return data;
     },
 
@@ -75,7 +43,7 @@ const HomeworkManager = {
     addAssignment(group, subject, text, files, author) {
         const data = this.loadHomework();
         if (!data[group]) {
-            data[group] = this.initializeGroupData();
+            data[group] = {};
         }
         if (!data[group][subject]) {
             data[group][subject] = {};
@@ -108,4 +76,7 @@ const HomeworkManager = {
         const data = this.loadHomework();
         return data[group] || {};
     }
-}; 
+};
+
+// Экспорт для использования в других файлах
+export default HomeworkManager; 
